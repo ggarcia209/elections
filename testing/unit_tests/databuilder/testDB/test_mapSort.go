acd9c07b7e54f5dc3410aaa1f60a9957d13e21d0
ggarcia209 <danielgarcia95367@gmail.com>
2020-02-13 17:42:56 -0800

    moved compare test to test_findTopX.go

--- testing/unit_tests/databuilder/testDB/test_mapSort.go
+++ testing/unit_tests/databuilder/testDB/test_mapSort.go
@@ -10,6 +10,7 @@ import (
 /* Unit Tests */
 var TestMap = map[string]float32{"indv01": 100, "indv02": 200, "indv03": 50, "indv04": 0, "indv05": 150}
 
+// SUCCESS
 func testSortTopX(m map[string]float32) Entries {
 	fmt.Println("sortTopX test")
 	es := sortTopX(m)
@@ -18,6 +19,7 @@ func testSortTopX(m map[string]float32) Entries {
 	return es
 }
 
+// SUCCESS
 func testSetThresholdLeastX(es Entries) (Entries, error) {
 	fmt.Println("setThresholdLeastX test")
 	th, err := setThresholdLeast3(es)
@@ -30,6 +32,7 @@ func testSetThresholdLeastX(es Entries) (Entries, error) {
 	return th, nil
 }
 
+// SUCCESS
 func testReSortLeast(es Entries) string {
 	fmt.Println("reSortLeast test")
 	fmt.Println("outside range -1")
@@ -90,6 +93,7 @@ func testReSortLeast(es Entries) string {
 	return delID
 }
 
+// SUCCESS
 func TestCompareUnits() error {
 	fmt.Println("testMap: ", TestMap)
 	es := testSortTopX(TestMap)
@@ -109,103 +113,7 @@ func printEntries(es Entries) {
 	fmt.Println()
 }
 
-func TestCompare() error {
-	// above least/empty threshold range
-	fmt.Println("comp1: above least/empty threshold")
-	comp1 := &comparison{
-		RefID:        "cmte01",
-		RefAmts:      map[string]float32{"indv01": 100, "indv02": 200, "indv03": 50, "indv04": 40, "indv05": 150},
-		RefTxs:       map[string]float32{"indv01": 1, "indv02": 1, "indv03": 1, "indv04": 1, "indv05": 1},
-		RefThreshold: []interface{}{},
-		CompID:       "indv06",
-		CompAmts:     map[string]float32{"cmte01": 300, "cmte02": 40},
-		CompTxs:      map[string]float32{"cmte01": 2, "cmte02": 1},
-	}
-	err := compare(comp1)
-	if err != nil {
-		return err
-	}
-	printComp(comp1)
-
-	// below least/empty threshold range
-	fmt.Println("comp2: below least/empty threshold")
-	comp2 := &comparison{
-		RefID:        "cmte01",
-		RefAmts:      map[string]float32{"indv01": 100, "indv02": 200, "indv03": 50, "indv04": 40, "indv05": 150},
-		RefTxs:       map[string]float32{"indv01": 1, "indv02": 1, "indv03": 1, "indv04": 1, "indv05": 1},
-		RefThreshold: []interface{}{},
-		CompID:       "indv06",
-		CompAmts:     map[string]float32{"cmte01": 10, "cmte02": 40},
-		CompTxs:      map[string]float32{"cmte01": 1, "cmte02": 1},
-	}
-	err = compare(comp2)
-	if err != nil {
-		return err
-	}
-	printComp(comp2)
-
-	// above least/non-empty threshold range
-	fmt.Println("comp3: above least/non-empty threshold")
-	comp3 := &comparison{
-		RefID:        "cmte01",
-		RefAmts:      map[string]float32{"indv01": 100, "indv02": 200, "indv03": 50, "indv04": 40, "indv05": 150},
-		RefTxs:       map[string]float32{"indv01": 1, "indv02": 1, "indv03": 1, "indv04": 1, "indv05": 1},
-		RefThreshold: []interface{}{&donations.Entry{"indv01", 100}, &donations.Entry{"indv03", 50}, &donations.Entry{"indv04", 0}},
-		CompID:       "indv06",
-		CompAmts:     map[string]float32{"cmte01": 300, "cmte02": 40},
-		CompTxs:      map[string]float32{"cmte01": 2, "cmte02": 1},
-	}
-	err = compare(comp3)
-	if err != nil {
-		return err
-	}
-	printComp(comp3)
-
-	// below least/non-empty threshold range
-	fmt.Println("comp4: below least/non-empty threshold")
-	comp4 := &comparison{
-		RefID:        "cmte01",
-		RefAmts:      map[string]float32{"indv01": 100, "indv02": 200, "indv03": 50, "indv04": 40, "indv05": 150},
-		RefTxs:       map[string]float32{"indv01": 1, "indv02": 1, "indv03": 1, "indv04": 1, "indv05": 1},
-		RefThreshold: []interface{}{&donations.Entry{"indv01", 100}, &donations.Entry{"indv03", 50}, &donations.Entry{"indv04", 0}},
-		CompID:       "indv06",
-		CompAmts:     map[string]float32{"cmte01": 10, "cmte02": 40},
-		CompTxs:      map[string]float32{"cmte01": 1, "cmte02": 1},
-	}
-	err = compare(comp4)
-	if err != nil {
-		return err
-	}
-	printComp(comp4)
-
-	return nil
-}
-
-func printComp(c *comparison) {
-	fmt.Println("RefID: ", c.RefID)
-	fmt.Println("RefAmts: ", c.RefAmts)
-	fmt.Println("RefTxs: ", c.RefTxs)
-	for _, e := range c.RefThreshold {
-		fmt.Printf("\t%s: %v", e.(*donations.Entry).ID, e.(*donations.Entry).Total)
-	}
-	fmt.Println()
-	fmt.Println("CompID: ", c.CompID)
-	fmt.Println("CompAmts: ", c.CompAmts)
-	fmt.Println("CompTxs: ", c.CompTxs)
-	fmt.Println()
-}
-
 /* Working Code */
-
-type comparison struct {
-	RefID        string             // reference object
-	RefAmts      map[string]float32 // marginal amount added to reference amount if compare amount > threshold
-	RefTxs       map[string]float32
-	RefThreshold []interface{}      // compare smallest amount in reference threshold list against compare amount
-	CompID       string             // object being compared to reference object
-	CompAmts     map[string]float32 // marginal amount included before comparison
-	CompTxs      map[string]float32
-}
 
 // Entries is a list of entries to be sorted.
 type Entries []*donations.Entry
@@ -223,83 +131,6 @@ func (s *Entries) popLeast() *donations.Entry {
 	del := a[len(a)-1]
 	*s = a[:len(a)-1]
 	return del
-}
-
-// compare compares the maps set in the comparison object to the threshold
-func compare(comp *comparison) error {
-	var least Entries
-	var err error
-
-	// if Threshold list is exhausted
-	if len(comp.RefThreshold) == 0 {
-		// sort Amts map and take bottom 10 as threshold list
-		es := sortTopX(comp.RefAmts)
-		least, err = setThresholdLeast3(es) // test only
-		if err != nil {
-			fmt.Println("compare failed: ", err)
-			return fmt.Errorf("compare failed: %v", err)
-		}
-	} else {
-		for _, entry := range comp.RefThreshold {
-			least = append(least, entry.(*donations.Entry))
-		}
-	}
-
-	// compare new sender's total to receiver's threshold value
-	threshold := least[len(least)-1].Total // last/smallest obj in least
-	fmt.Println("least: ", threshold)
-
-	// if amount sent to receiver is > receiver's threshold
-	if comp.CompAmts[comp.RefID] > threshold {
-		// create new threshold entry for sender & amount contributed by sender
-		new := newEntry(comp.CompID, comp.CompAmts[comp.RefID])
-		// reSort threshold list w/ new entry and retreive deletion key for obj below threshold
-		delID := reSortLeast(new, &least)
-		// delete the records for obj below threshold
-		delete(comp.RefAmts, delID)
-		delete(comp.RefTxs, delID)
-		// add new obj data to records
-		comp.RefAmts[comp.CompID] = comp.CompAmts[comp.RefID]
-		comp.RefTxs[comp.CompID] = comp.CompTxs[comp.RefID]
-	} else {
-		// sender/value does not qualify -- return and continue
-		return nil
-	}
-
-	// update object's threshold list
-	th := []interface{}{}
-	for _, entry := range least {
-		th = append(th, entry)
-	}
-	comp.RefThreshold = append(comp.RefThreshold[:0], th...)
-
-	return nil
-}
-
-// check to see if previous total of entry is in threshold range when updating existing entry
-func checkThreshold(newID string, m map[string]float32, th []interface{}) ([]interface{}, error) {
-	inRange := false
-	check := map[string]bool{newID: true}
-	for _, e := range th {
-		if check[e.(*donations.Entry).ID] == true {
-			inRange = true
-		}
-	}
-	if inRange {
-		es := sortTopX(m)
-		newRange, err := setThresholdLeast3(es)
-		if err != nil {
-			fmt.Println("checkThreshold failed: ", err)
-			return []interface{}{}, fmt.Errorf("checkThreshold failed: %v", err)
-		}
-		// update object's threshold list
-		newTh := []interface{}{}
-		for _, entry := range newRange {
-			newTh = append(newTh, entry)
-		}
-		return newTh, nil
-	}
-	return th, nil
 }
 
 // reSortLeast re-sorts the least 5 or 10 values when a new value breaks the threshold (least[len(least)-1].Total)
